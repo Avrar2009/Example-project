c8495c78120ede2f6e4044b8ab3799efed95c32d
Avrar2009 <julekhajoya3@gmail.com>
2022-05-23 23:06:35 -0400

    test

---
+++ src/test/java/p4/Arraylist.java
@@ -0,0 +1,10 @@
+package p4;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		//
+
+	}
+
+}
---
+++ src/test/java/p4/Lcep4.java
@@ -0,0 +1,30 @@
+package p4;
+
+public class Lcep4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//String fun
+		String fun="java for fun";
+		for(int i=0;i<=4;i++) {
+		System.out.print(" java for fun +  ");
+		}	
+		//program to find the sum of numbers from 1 to 100
+		//for loop
+		
+		int sum=0;
+		
+	for(int i=0;i<=100;i++) {	
+		//body inside for loop
+		
+		sum += i;//sum+sum+i
+	
+		System.out.println("sum="+ sum);
+	}
+		
+		
+		
+		  
+	}
+
+}
